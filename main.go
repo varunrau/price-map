098d68d127a3f3b67296d8125b143d74606033a1
Varun Rau <varunrau@gmail.com>
2018-01-30 20:57:41 -0800

    reads secrets from yaml

--- main.go
+++ main.go
@@ -7,8 +7,17 @@ import (
 	"encoding/json"
 	"golang.org/x/net/context"
 	"googlemaps.github.io/maps"
+	yaml "gopkg.in/yaml.v2"
 	"io/ioutil"
 )
+
+type Config struct {
+	GoogleMaps GoogleMapsConfig `yaml:"google-maps"`
+}
+
+type GoogleMapsConfig struct {
+	APIKey string `yaml:"api"`
+}
 
 type CensusTracts struct {
 	Meta struct{}  `json:"meta"`
@@ -32,13 +41,20 @@ func main() {
 		log.Fatalf("failed to read census data %s", err)
 	}
 
+	rawYaml, err := ioutil.ReadFile("./secrets.yaml")
+	if err != nil {
+		log.Fatalf("failed to read secrets.yaml %s", err)
+	}
+	config := Config{}
+	yaml.Unmarshal(rawYaml, &config)
+
 	var censusData CensusTracts
 	err = json.Unmarshal(raw, &censusData)
 	if err != nil {
 		log.Fatalf("failed to unmarshal census data %s", err)
 	}
 
-	c, err := maps.NewClient(maps.WithAPIKey(""))
+	c, err := maps.NewClient(maps.WithAPIKey(config.GoogleMaps.APIKey))
 	if err != nil {
 		log.Fatalf("fatal error: %s", err)
 	}
@@ -47,7 +63,6 @@ func main() {
 		data := censusData.Data[i+1]
 		for j := range censusData.Data[2:] {
 			toData := censusData.Data[j+2]
-			fmt.Println(fmt.Sprintf("Lat: %s, Long: %s \nLat: %s, Long: %s \n\n", data.GetLatitude(), data.GetLongitude(), toData.GetLatitude(), toData.GetLongitude()))
 			getRoute(c, data.GetLatitude(), data.GetLongitude(), toData.GetLatitude(), toData.GetLongitude())
 		}
 	}
